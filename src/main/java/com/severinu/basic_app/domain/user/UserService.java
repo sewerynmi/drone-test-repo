78d1b541a77ef38691cf0ca77189bde9855d207d
Seweryn Michota <Seweryn.Michota@digital.homeoffice.gov.uk>
2022-08-02 01:53:41 +0100

    update  User entity

--- src/main/java/com/severinu/basic_app/domain/user/UserService.java
+++ src/main/java/com/severinu/basic_app/domain/user/UserService.java
@@ -6,7 +6,7 @@ import org.springframework.stereotype.Service;
 public class UserService {
 
     public User getUser() {
-        return new User(1L, "Adam", "Smithh");
+        return new User(1L, "Adam", "Smith");
     }
 
 }
