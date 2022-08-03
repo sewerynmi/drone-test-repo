f6fe8d4d1ad13aa4a9b82b8da21b17a048c1611c
Seweryn Michota <Seweryn.Michota@digital.homeoffice.gov.uk>
2022-08-03 19:21:39 +0100

    remove getUser from UserRepo

--- src/main/java/com/severinu/basic_app/domain/user/UserRepository.java
+++ src/main/java/com/severinu/basic_app/domain/user/UserRepository.java
@@ -5,7 +5,5 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class UserRepository {
 
-    public User getUser() {
-        return new User(2L,"Thomas","Catnip");
-    }
+
 }
