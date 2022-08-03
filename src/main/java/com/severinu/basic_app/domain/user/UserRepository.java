82fd3e21294de0be0e1005354a350056a44be827
Seweryn Michota <Seweryn.Michota@digital.homeoffice.gov.uk>
2022-08-03 19:24:23 +0100

    Add getUser to UserRepo

--- src/main/java/com/severinu/basic_app/domain/user/UserRepository.java
+++ src/main/java/com/severinu/basic_app/domain/user/UserRepository.java
@@ -5,5 +5,7 @@ import org.springframework.stereotype.Repository;
 @Repository
 public class UserRepository {
 
-
+    public User getUser() {
+        return new User(2L,"Thomas","Catnip");
+    }
 }
