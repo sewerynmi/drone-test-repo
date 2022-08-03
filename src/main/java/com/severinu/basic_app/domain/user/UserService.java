a5bddd2c7621eb75bc6ac97cae3b83849414a82d
Seweryn Michota <Seweryn.Michota@digital.homeoffice.gov.uk>
2022-08-03 22:22:18 +0100

    Add Basic failed test

--- src/main/java/com/severinu/basic_app/domain/user/UserService.java
+++ src/main/java/com/severinu/basic_app/domain/user/UserService.java
@@ -5,8 +5,14 @@ import org.springframework.stereotype.Service;
 @Service
 public class UserService {
 
+    private final UserRepository userRepository;
+
+    public UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     public User getUser() {
-        return new User(1L, "Adam", "Smith");
+        return userRepository.getUser();
     }
 
 }
--- src/test/java/com/severinu/basic_app/BasicAppApplicationTests.java
+++ src/test/java/com/severinu/basic_app/BasicAppApplicationTests.java
@@ -3,11 +3,18 @@ package com.severinu.basic_app;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.fail;
+
 @SpringBootTest
 class BasicAppApplicationTests {
 
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    void failedTest () {
+        fail("This test should fail.");
     }
 
 }
